1b19c9ffdc19927ad261869aa9cd5e2ba112780d
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-10-24 15:06:51 +0300

    Simple variable dos and donts

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,42 @@
 console.log("Console statement coming from imported script");
+
+// Just values
+console.log("Jonas ", "string");
+console.log(8, " number");
+
+// Assigning constant values to variables
+const name = "Jonas";
+const age = 8;
+
+// Checking type of values
+console.log(typeof name);
+console.log(typeof age);
+
+// Using variables that have values
+
+console.log(name, " is ", age, " years old.");
+
+//Interpolated string
+
+console.log(`${name} is ${age} years old.`);
+
+// camelCase for variables
+
+const firstName = "John";
+
+// Constants that will never chnage -> UPPERCASE
+
+const PI = 3.1415;
+
+// Functions
+
+let $dosth = () => {
+  console.log("Doing sth");
+};
+
+$dosth();
+
+let myFunctionDoesThings = () => {
+  console.log("Doing things");
+};
+myFunctionDoesThings();

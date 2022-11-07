a7c45182cf5e535b196ec129e12acf8f4fb836db
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-11-07 16:53:20 +0200

    Add keydown, escape key logic

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+// "use Strict";
+// // Activate Strict Mode -> better for debugging (visible errors in console)
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) {
+//   hasDriversLicence = true; // if we misspell, strict Mode will help
+// }
+
+// KEY PRESS EVENTS
+
+// document.addEventListener("keydown", (e) => {
+//   console.log("Key pressed");
+//   console.log(e.code === "Escape" ? "Hit escape key" : "Not escape key");
+// });
